0b54e7cd524166e5c9f96ba30ffd7ba50a653d94
Priyanka Balivada <balivadapriyanka2519@gmail.com>
2023-05-07 12:06:48 +0530

    Add reports generate feature

--- CardioInsight.py
+++ CardioInsight.py
@@ -3,6 +3,12 @@ import pandas as pd;
 import numpy as np;
 import pickle;
 from streamlit_option_menu import option_menu
+from fpdf import FPDF
+import base64
+
+def create_download_link(val, filename):
+    b64 = base64.b64encode(val)  # val looks like b'...'
+    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
 
 div_html = """
 <div style='display:flex; justify-content:center; align-items:center'>
@@ -22,17 +28,17 @@ with st.sidebar:
 if selected == "Heart":
     
     safe_html="""  
-          <div style="background-color:green;padding:11px;margin-top:10px" >
+          <div style="background-color:green;padding:11px;margin-top:10px;margin-bottom:10px" >
            <h2 style="color:white;text-align:center;"> Your heart is safe</h2>
            </div>"""
 
     danger_html="""  
-          <div style="background-color:red;padding:11px;margin-top:10px" >
+          <div style="background-color:red;padding:11px;margin-top:10px;margin-bottom:10px" >
            <h2 style="color:white;text-align:center;"> Your heart is in danger</h2>
            </div>"""
 
     html_temp = """
-          <div style="background-color:tomato;padding:20px;margin-bottom:30px" >
+          <div style="background-color:tomato;padding:20px;margin-bottom:30px;margin-bottom:10px" >
         <h2 style = "color:white; text-align:center;">Disease Prediction Model -(Heart) </h2>
         </div>
         """
@@ -64,15 +70,41 @@ if selected == "Heart":
         else:
             st.markdown(danger_html,unsafe_allow_html=True)
 
+    st.write();
+
+    export_as_pdf = st.button("Export Report")
+
+    if export_as_pdf:
+        pdf = FPDF()
+        pdf.add_page()
+        pdf.set_font('Arial', 'B', 25)
+        pdf.cell(200, 30, txt="Heart Diagnose", ln=1, align='C')
+        pdf.set_font('Arial', '', 16)
+        pdf.cell(40, 10, "Cholesterol : "+str(f1), ln=1)
+        pdf.cell(40, 10, "MaxHR : "+str(f2), ln=1)
+        pdf.cell(40, 10, "Oldpeak : "+str(f3), ln=1)
+        pdf.cell(40, 10, "ChestPainType : "+str(f4), ln=1)
+        pdf.cell(40, 10, "ExerciseAngina : "+str(f5), ln=1)
+        pdf.cell(40, 10, "Age : "+str(f6), ln=1)
+        pdf.cell(40, 10, "ST_Slope : "+str(f7), ln=1)
+        if prediction == 0:
+            pdf.cell(150,30,"Your heart is safe", ln=1, align='C')   
+        else:
+            pdf.cell(150,30,"Your heart is in danger", ln=1, align='C')   
+
+        html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
+
+        st.markdown(html, unsafe_allow_html=True)
+
 if selected == "Diabetes":
     
     safe_html="""  
-          <div style="background-color:green;padding:11px;margin-top:10px" >
+          <div style="background-color:green;padding:11px;margin-top:10px;margin-bottom:10px" >
            <h2 style="color:white;text-align:center;">You are safe from Diabetes!! </h2>
            </div>"""
 
     danger_html="""  
-          <div style="background-color:red;padding:11px;margin-top:10px" >
+          <div style="background-color:red;padding:11px;margin-top:10px;margin-bottom:10px" >
            <h2 style="color:white;text-align:center;"> You seem to have Diabetes..</h2>
            </div>"""
 
@@ -109,20 +141,46 @@ if selected == "Diabetes":
         else:
             st.markdown(danger_html,unsafe_allow_html=True)
 
+    st.write();
+
+    export_as_pdf = st.button("Export Report")
+
+    if export_as_pdf:
+        pdf = FPDF()
+        pdf.add_page()
+        pdf.set_font('Arial', 'B', 25)
+        pdf.cell(200, 30, txt="Diabetes Diagnose", ln=1, align='C')
+        pdf.set_font('Arial', '', 16)
+        pdf.cell(40, 10, "Insulin : "+str(f1), ln=1)
+        pdf.cell(40, 10, "Glucose : "+str(f2), ln=1)
+        pdf.cell(40, 10, "Age : "+str(f3), ln=1)
+        pdf.cell(40, 10, "Pregnancies : "+str(f4), ln=1)
+        pdf.cell(40, 10, "BMI : "+str(f5), ln=1)
+        pdf.cell(40, 10, "Skin Thickness : "+str(f6), ln=1)
+        pdf.cell(40, 10, "Diabetes Pedegree Function : "+str(f7), ln=1)
+        if prediction == 0:
+            pdf.cell(150,30,"You are safe from Diabetes!! ", ln=1, align='C')   
+        else:
+            pdf.cell(150,30,"You seem to have Diabetes..", ln=1, align='C')   
+
+        html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
+
+        st.markdown(html, unsafe_allow_html=True)
+
 if selected == "Lung Cancer":
     
     lvl0_html="""
-        <div style="background-color:green;padding:11px;margin-top:10px" >
+        <div style="background-color:green;padding:11px;margin-top:10px;margin-bottom:10px" >
         <h2 style="color:white;text-align:center;">You are having LOW chances of having cancer</h2>
         </div>"""
 
     lvl1_html="""  
-          <div style="background-color:yellow;padding:11px;margin-top:10px" >
+          <div style="background-color:yellow;padding:11px;margin-top:10px;margin-bottom:10px" >
            <h2 style="color:black;text-align:center;">You are having MEDIUM chances of having cancer</h2>
            </div>"""
     
     lvl2_html="""  
-          <div style="background-color:red;padding:11px;margin-top:10px" >
+          <div style="background-color:red;padding:11px;margin-top:10px;margin-bottom:10px" >
            <h2 style="color:white;text-align:center;">You are having HIGH chances of having cancer</h2>
            </div>"""
 
@@ -160,6 +218,34 @@ if selected == "Lung Cancer":
             st.markdown(lvl0_html,unsafe_allow_html=True)
         else:
             st.markdown(lvl1_html, unsafe_allow_html=True)
+
+    st.write();
+
+    export_as_pdf = st.button("Export Report")
+
+    if export_as_pdf:
+        pdf = FPDF()
+        pdf.add_page()
+        pdf.set_font('Arial', 'B', 25)
+        pdf.cell(200, 30, txt="Lung Cancer Diagnose", ln=1, align='C')
+        pdf.set_font('Arial', '', 16)
+        pdf.cell(40, 10, "Coughing of Blood : "+str(f1), ln=1)
+        pdf.cell(40, 10, "Alcohol Use : "+str(f2), ln=1)
+        pdf.cell(40, 10, "Passive Smoker : "+str(f3), ln=1)
+        pdf.cell(40, 10, "Obesity : "+str(f4), ln=1)
+        pdf.cell(40, 10, "Smoking : "+str(f5), ln=1)
+        pdf.cell(40, 10, "Balanced Diet : "+str(f6), ln=1)
+        pdf.cell(40, 10, "Chest Pain : "+str(f7), ln=1)
+        if prediction == 0:
+            pdf.cell(200,30,"You are having HIGH chances of having cancer", ln=1, align='C')    
+        elif prediction == 1:
+            pdf.cell(200,30,"You are having LOW chances of having cancer", ln=1, align='C') 
+        else:
+            pdf.cell(200,30,"You are having MEDIUM chances of having cancer", ln=1, align='C')   
+
+        html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
+
+        st.markdown(html, unsafe_allow_html=True)
 
 
 
